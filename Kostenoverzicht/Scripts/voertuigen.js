1b0e25bb9417f4ee4184a8300e77eecdd03a3215
Piet2001 <piet2001@fireman.net>
2021-10-20 21:47:13 +0200

    Overige voertuigen toegevoegd

--- Kostenoverzicht/Scripts/voertuigen.js
+++ Kostenoverzicht/Scripts/voertuigen.js
@@ -26,3 +26,32 @@ fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/main/Kosteno
     )
   }
 )
+
+// overige brandweer voertuigen
+fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/main/Kostenoverzicht/Data/OverigeVoertuigen.json").then(
+  res => {
+    res.json().then(
+      data => {
+        console.log(data);
+        console.log(data.length);
+        if (data.length > 0) {
+
+          var temp = "";
+          data.forEach((itemData) => {
+            console.log(itemData)
+            temp += "<tr>";
+            temp += "<td>" + `<img src=${itemData.afbeelding} />` + "</td>";
+            temp += "<td>" + itemData.afkorting + "</td>";
+            temp += "<td>" + itemData.omschrijving + "</td>";
+            temp += "<td>" + itemData.vanaf + "</td>";
+            temp += "<td>" + itemData.kosten + "</td>";
+            temp += "<td>" + itemData.opleiding + "</td>";
+            temp += "<td>" + itemData.duur + "</td>";
+            temp += "<td>" + itemData.personeel + "</td></tr>";
+          });
+          document.getElementById('OverigeVoertuigen').innerHTML = temp;
+        }
+      }
+    )
+  }
+)
