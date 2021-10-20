d427da62a120441242e403e43a52ebd23a02b22c
Piet2001 <piet2001@fireman.net>
2021-10-20 22:29:55 +0200

    Politie Toegevoegd

--- Kostenoverzicht/Scripts/voertuigen.js
+++ Kostenoverzicht/Scripts/voertuigen.js
@@ -127,3 +127,29 @@ fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/main/Kosteno
     )
   }
 )
+
+// Politie
+fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/main/Kostenoverzicht/Data/Politievoertuigen.json").then(
+  res => {
+    res.json().then(
+      data => {
+        if (data.length > 0) {
+          var temp = "";
+          data.forEach((itemData) => {
+            console.log(itemData)
+            temp += "<tr>";
+            temp += "<td>" + `<img src=${itemData.afbeelding} />` + "</td>";
+            temp += "<td>" + itemData.afkorting + "</td>";
+            temp += "<td>" + itemData.omschrijving + "</td>";
+            temp += "<td>" + itemData.vanaf + "</td>";
+            temp += "<td>" + itemData.kosten + "</td>";
+            temp += "<td>" + itemData.opleiding + "</td>";
+            temp += "<td>" + itemData.duur + "</td>";
+            temp += "<td>" + itemData.personeel + "</td></tr>";
+          });
+          document.getElementById('Politievoertuigen').innerHTML = temp;
+        }
+      }
+    )
+  }
+)
