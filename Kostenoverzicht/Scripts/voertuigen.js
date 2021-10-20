64137b861e8edfeb1acac76997a337b61c1ceba4
Piet2001 <piet2001@fireman.net>
2021-10-20 22:07:29 +0200

    Waterongevallen toegevoegd

--- Kostenoverzicht/Scripts/voertuigen.js
+++ Kostenoverzicht/Scripts/voertuigen.js
@@ -75,3 +75,29 @@ fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/main/Kosteno
     )
   }
 )
+
+// Waterongevallen
+fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/main/Kostenoverzicht/Data/Waterongevallen.json").then(
+  res => {
+    res.json().then(
+      data => {
+        if (data.length > 0) {
+          var temp = "";
+          data.forEach((itemData) => {
+            console.log(itemData)
+            temp += "<tr>";
+            temp += "<td>" + `<img src=${itemData.afbeelding} />` + "</td>";
+            temp += "<td>" + itemData.afkorting + "</td>";
+            temp += "<td>" + itemData.omschrijving + "</td>";
+            temp += "<td>" + itemData.vanaf + "</td>";
+            temp += "<td>" + itemData.kosten + "</td>";
+            temp += "<td>" + itemData.opleiding + "</td>";
+            temp += "<td>" + itemData.duur + "</td>";
+            temp += "<td>" + itemData.personeel + "</td></tr>";
+          });
+          document.getElementById('Waterongevallen').innerHTML = temp;
+        }
+      }
+    )
+  }
+)
