46c067c2d39bbdb04670a899bd4d60e047dafea5
Piet2001 <piet2001@fireman.net>
2021-10-21 10:11:15 +0200

    Missende voertuigen toegevoegd

--- Kostenoverzicht/Scripts/voertuigen.js
+++ Kostenoverzicht/Scripts/voertuigen.js
@@ -179,3 +179,29 @@ fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/main/Kosteno
     )
   }
 )
+
+// Waterredding
+fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/main/Kostenoverzicht/Data/Waterreddingsvoertuigen.json").then(
+  res => {
+    res.json().then(
+      data => {
+        if (data.length > 0) {
+          var temp = "";
+          data.forEach((itemData) => {
+            console.log(itemData)
+            temp += "<tr>";
+            temp += "<td>" + `<img src=${itemData.afbeelding} />` + "</td>";
+            temp += "<td>" + itemData.afkorting + "</td>";
+            temp += "<td>" + itemData.omschrijving + "</td>";
+            temp += "<td>" + itemData.vanaf + "</td>";
+            temp += "<td>" + itemData.kosten + "</td>";
+            temp += "<td>" + itemData.opleiding + "</td>";
+            temp += "<td>" + itemData.duur + "</td>";
+            temp += "<td>" + itemData.personeel + "</td></tr>";
+          });
+          document.getElementById('Waterreddingsvoertuigen').innerHTML = temp;
+        }
+      }
+    )
+  }
+)
