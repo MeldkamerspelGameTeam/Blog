f29836fe93e6dab14c06fb17100a9fdcf6c4d8df
Piet2001 <piet2001@fireman.net>
2023-04-16 16:53:26 +0200

    update

---
+++ Kostenoverzichtv2/Scripts/voertuigen.js
@@ -0,0 +1,100 @@
+const VoertuigGroepen = [
+    {
+        "name": "Blusvoertuigen",
+        "display": "Blusvoertuigen",
+        "table": "Rood"
+    },
+    {
+        "name": "Overige voertuigen",
+        "display": "Overige voertuigen",
+        "table": "Rood"
+    },
+    {
+        "name": "Haakarm",
+        "display": "Haakarmbakken (containers)",
+        "table": "Rood"
+    },
+    {
+        "name": "waterongevallen",
+        "display": "Waterongevallen",
+        "table": "Rood"
+    },
+    {
+        "name": "vbb",
+        "display": "Vliegtuigbrandbestrijding",
+        "table": "Rood"
+    },
+    {
+        "name": "politie",
+        "display": "Politievoertuigen",
+        "table": "Blauw"
+    },
+    {
+        "name": "ambu",
+        "display": "Ambulancevoertuigen",
+        "table": "Geel"
+    },
+    {
+        "name": "kustwacht",
+        "display": "Kustwacht",
+        "table": "Oranje"
+    }
+]
+
+var voertuigen = []
+
+fetch("https://raw.githubusercontent.com/MeldkamerspelGameTeam/Blog/kostenv2/Kostenoverzichtv2/Scripts/Data/voertuigen.json").then(
+    res => {
+        res.json().then(
+            data => {
+                if (data.length > 0) {
+                    voertuigen = data;
+                    Verwerkvoertuigen();
+                }
+            }
+        )
+    }
+)
+
+function Verwerkvoertuigen() {
+    html = "";
+
+    for (var groep in VoertuigGroepen) {
+        html += "<h2>" + VoertuigGroepen[groep].display + "</h2>"
+        html += "<table class='table' id='" + VoertuigGroepen[groep].table + "'>"
+        html += "<thead>"
+        html += "<tr>"
+        html += "<th>Afbeelding</th>"
+        html += "<th>Afkorting</th>"
+        html += "<th>Omschrijving</th>"
+        html += "<th>Vanaf</th>"
+        html += "<th>Kosten</th>"
+        html += "<th>Opleiding</th>"
+        html += "<th>Duur</th>"
+        html += "<th>Pers.</th>"
+        html += "</tr >"
+        html += "</thead >"
+        html += "<tbody>"
+        var voertuigenInGroep = voertuigen.filter(v => v.groep === VoertuigGroepen[groep].name)
+        var temp = "";
+        voertuigenInGroep.forEach((voertuig) => {
+            temp += "<tr>";
+            temp += "<td>" + `<img src=${voertuig.afbeelding}></img >` + "</td>";
+            temp += "<td>" + voertuig.afkorting + "</td>";
+            temp += "<td>" + voertuig.omschrijving + "</td>";
+            temp += "<td>" + voertuig.vanaf + "</td>";
+            temp += "<td>" + voertuig.kosten + "</td>";
+            temp += "<td>" + voertuig.opleiding + "</td>";
+            temp += "<td>" + voertuig.duur + "</td>";
+            temp += "<td>" + voertuig.personeel + "</td></tr>";
+        });
+        html += temp
+
+        html += "</tbody >"
+        html += "</table >"
+    }
+
+    document.getElementById('mksvehicles').innerHTML = html;
+
+}
+
